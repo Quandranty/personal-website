971082ce6eb54785c0e4d083f8f75c7f28133b19
Quandranty <101238321+Quandranty@users.noreply.github.com>
2022-04-15 09:46:44 +0800

    My Personal Website

---
+++ app.js
@@ -0,0 +1,10 @@
+const facebook_btn = document.querySelector(".facebook-btn")
+const github_btn = document.querySelector(".gihub-btn")
+
+facebook_btn.addEventListener('click', function(){
+    window.open("https://www.facebook.com/Frank.Sterilyz")
+})
+
+github_btn.addEventListener("click", function(){
+    window.open("https://github.com/debuunked")
+})
